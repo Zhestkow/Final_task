ec90b9b8e1cfe2bb3581efec3eb5e74fa8562a8a
Your Name <you@example.com>
2022-11-10 20:51:54 +0300

    Start

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите значения через пробел: ");
+string[] Array = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+
+
+
