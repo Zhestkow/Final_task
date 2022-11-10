a9d8b5d498c85685f21d0a6b92b55180b14b68df
Your Name <you@example.com>
2022-11-10 20:53:51 +0300

    Print first array

--- Program.cs
+++ Program.cs
@@ -4,8 +4,17 @@ Clear();
 
 Write("Введите значения через пробел: ");
 string[] Array = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+PrintNewArray(Array);
 
 
 
 
-
+void PrintNewArray(string[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length - 1; i++)
+    {
+        Write($"{inArray[i]},");
+    }
+    Write($"{inArray[inArray.Length - 1]}]");
+}
